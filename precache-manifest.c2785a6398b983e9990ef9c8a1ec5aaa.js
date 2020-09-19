c49f536325c94d25ed9d2ddc3ea36cc407816b1e
Raymond Chan <cwh.raymond@gmail.com>
2020-09-20 00:07:34 +0800

    inital repo

---
+++ precache-manifest.c2785a6398b983e9990ef9c8a1ec5aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3228ca1c85efd1f59d6b4cc58e94c79b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7851a444e503c1edd6a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61db003b46cd32706113",
+    "url": "/static/js/2.5a63e2f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a63e2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7851a444e503c1edd6a",
+    "url": "/static/js/main.06c25684.chunk.js"
+  },
+  {
+    "revision": "4170743fd322251a69e1",
+    "url": "/static/js/runtime-main.66647b44.js"
+  }
+]);
